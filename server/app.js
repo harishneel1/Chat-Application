683e458957aea70bd6fdf382d419270464e166ae
NucleusHQ <hi@nucleushq.io>
2024-06-29 11:16:14 +0530

    Final touch ups

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
-const { Server } = require("socket.io");
-const http = require("http");
+import { Server } from "socket.io"
+import express from "express"
 
-const httpServer = http.createServer();
+const app = express();
 
-httpServer.listen(3001);
+const PORT = process.env.PORT || 3001;
+
+const httpServer = app.listen(PORT, () => {
+    console.log(`Server is now listening to PORT ${PORT}`)
+});
+
 
 const io = new Server(httpServer, {
     cors: {
@@ -14,6 +19,24 @@ const io = new Server(httpServer, {
 io.on("connection", (socket) => {
 
     console.log(`User ${socket.id} connected`);
+
+    // Upon connection - only to current user
+    socket.emit("message", "Welcome to Chat App");
+
+    // Upon connection - to all other users
+    socket.broadcast.emit("message", `User ${socket.id.substring(0, 5)} connected`)
+
+    // Upon disconnection - to all other users
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("message", `User ${socket.id.substring(0, 5)} disconnected`)
+    })
+
+    // capturing the activity event
+
+    socket.on("activity", (name) => {
+        socket.broadcast.emit("activity", name);
+    })
 
     socket.on("message", (data) => {
         console.log(data);
